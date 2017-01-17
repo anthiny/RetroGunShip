a2cba769b54d6082cfc409dede7717bc8d54d942
anthony <egg499@naver.com>
2017-01-17 20:14:15 +0900

    first

---
+++ Assets/Scripts/BulletAi.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletAi : MonoBehaviour {
+	public int damage;
+	public float h;
+	public float w;
+	void Start(){
+	}
+	void Update(){
+		if(transform.position.x > w || transform.position.y > h
+		|| transform.position.x < - w || transform.position.y < -h){
+			Destroy(gameObject);
+		}
+	}
+
+	public void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.CompareTag("Player")){
+			col.gameObject.GetComponent<Player>().Damage(damage);
+			Destroy(gameObject);
+		}
+	}
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 0.1f;
+	public bool doubleSpeed = false;
+	public int curHp = 5;
+	private Animator animator;
+	private bool isLeft;
+	private bool isRight;
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetBool("isLeft",isLeft);
+		animator.SetBool("isRight",isRight);
+		if (Input.GetAxis ("Horizontal") < -0.1f){
+			isLeft = true;
+			isRight = false;
+			transform.position = new Vector3(transform.position.x - (speed * Time.deltaTime), transform.position.y, 0);
+		}
+		if (Input.GetAxis ("Horizontal") > 0.1f) {
+			isRight = true;
+			isLeft = false;
+			transform.position = new Vector3(transform.position.x + (speed * Time.deltaTime), transform.position.y, 0);
+		}
+		if(Input.GetAxis("Vertical") > 0.1f){
+			if(Input.GetAxis ("Horizontal") < -0.1f){
+				isLeft = true;
+				isRight = false;
+			}
+			else if(Input.GetAxis ("Horizontal") > 0.1f){
+				isLeft = false;
+				isRight = true;
+			}
+			else{
+				isLeft = false;
+				isRight = false;
+			}
+			transform.position = new Vector3(transform.position.x, transform.position.y + (speed * Time.deltaTime), 0);
+		}
+		if(Input.GetAxis("Vertical") < -0.1f){
+			if(Input.GetAxis ("Horizontal") < -0.1f){
+				isLeft = true;
+				isRight = false;
+			}
+			else if(Input.GetAxis ("Horizontal") > 0.1f){
+				isLeft = false;
+				isRight = true;
+			}
+			else{
+				isLeft = false;
+				isRight = false;
+			}
+			transform.position = new Vector3(transform.position.x, transform.position.y - (speed * Time.deltaTime), 0);
+		}
+		if(Input.GetAxis("Vertical") == 0.0f && Input.GetAxis("Horizontal") == 0.0f){
+			isLeft = false;
+			isRight = false;	
+		}
+	}
+	public void Damage(int damage){
+		if(curHp <= 0){
+			Die();
+		}
+		curHp = curHp - damage;
+		animator.SetTrigger("Heat");
+	}
+	void Die(){
+		//Restart
+		SceneManager.LoadScene("InGame");
+	}
+
+}
