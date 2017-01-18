988abcc8ee4909f364568f3ece6533c788990e47
anthony <egg499@naver.com>
2017-01-18 21:04:31 +0900

    add pasuemenu n score

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour {
+
+	private float lastUpdate;
+	private int score;
+	public Text pointsText;
+	void Start(){
+		score = 0;
+	} 
+	void Update(){
+		if(Time.time - lastUpdate >= 1f){
+			lastUpdate = Time.time;
+			score += 1;
+			displayScore(score);
+		}
+	}
+
+	public void displayScore(int score){
+		pointsText.text = ("Score: " + score);
+	}
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+	public GameObject PauseUI;
+
+	private bool paused = false;
+
+	void Start(){
+		PauseUI.SetActive (false);
+	}
+
+	void Update(){
+		if (Input.GetButtonDown ("Pause")) {
+			paused = !paused;
+		}
+		if (paused) {
+			PauseUI.SetActive (true);
+			Time.timeScale = 0;
+		} else {
+			PauseUI.SetActive (false);
+			Time.timeScale = 1;
+		}
+	}
+
+	public void Resume(){
+		paused = false;
+	}
+
+	public void Restart(){
+		SceneManager.LoadScene ("InGame");
+	}
+
+	public void MainMenu(){
+	}
+
+	public void Quit(){
+		Application.Quit ();
+	}
+}
